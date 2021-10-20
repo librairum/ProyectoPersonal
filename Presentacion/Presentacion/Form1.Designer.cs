c0d8088890f053fba23280c46c6661fa66910461
CesarHR-Git <cesarhidalgo_rasec@hotmail.com>
2021-10-20 10:31:23 -0500

    Prueba de conflictos
    Prueba de conflictos

--- Presentacion/Presentacion/Form1.Designer.cs
+++ Presentacion/Presentacion/Form1.Designer.cs
@@ -163,6 +163,7 @@
             this.button2.TabIndex = 12;
             this.button2.Text = "Proyecto2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
