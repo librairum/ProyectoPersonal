833c5b8c21ae5dcf1b8d264249642a9047bfd58f
CesarHR-Git <cesarhidalgo_rasec@hotmail.com>
2021-12-02 15:25:50 -0500

    prueba-2 CHR

---
+++ Presentacion/Testing-CHR/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testing_CHR
+{
+    public class Class1
+    {
+        //params
+        int id;
+        int name;
+    }
+}
